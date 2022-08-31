fa8ff9677b5c70a73b2b04993816865a9bc2026f
Aljwhra <91431846+Aljwhra@users.noreply.github.com>
2022-08-31 22:31:30 +0300

    Delete jokes directory

--- jokes/server/config/mongoose.config.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://localhost/jokes", {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-})
-	.then(() => console.log("succesfully connected to the database"))
-	.catch((err) => console.log("Failed to connect to the database "));
